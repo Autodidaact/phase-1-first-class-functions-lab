12b20c6643f9ae9f3c23b79a53b3538fe8b4bcdf
Ibrahim Abdulai Yakubu <ibrahimay295@gmail.com>
2022-06-10 18:15:04 +0000

    first class function lab fixed

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+const returnFirstTwoDrivers = () => {return drivers.slice(0, 2)};
+
+const returnLastTwoDrivers = () => {return drivers.slice(-2)};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+ 
+function createFareMultiplier(fare) {
+    return (fare) => {
+        return fare * fare;
+    }
+
+    
+}
+
+const fareDoubler = (fare) => {
+    return fare * 2;
+}
+const fareTripler = (fare) => {
+    return fare * 3;
+}
+function selectDifferentDrivers(drivers, func){
+    if (func == returnFirstTwoDrivers){
+        return returnFirstTwoDrivers();
+    }else if (func == returnLastTwoDrivers){
+        return returnLastTwoDrivers();
+    }
+}
